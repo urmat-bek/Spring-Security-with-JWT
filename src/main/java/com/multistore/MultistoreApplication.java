fb39df238c3b1fd18b52f8279fb6386e0ad357fb
Urmatbek Asilbekov <asilbekovurmatbek04@gmail.com>
2024-07-10 10:58:32 +0600

    first commit

---
+++ src/main/java/com/multistore/MultistoreApplication.java
@@ -0,0 +1,13 @@
+package com.multistore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultistoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultistoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multistore/MultistoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multistore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultistoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
